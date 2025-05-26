038b56d4819ac50b2b6043a20e65196517988adc
Patrick Rodrigues (rodapdev) <47316946+RodPaDev@users.noreply.github.com>
2025-05-26 10:58:08 +0100

    rename to mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,10 @@ import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default tseslint.config(
   js.configs.recommended,
--- libs/types/eslint.config.mjs
+++ libs/types/eslint.config.mjs
@@ -1,4 +1,4 @@
-import baseConfig from '../../eslint.config.js';
+import baseConfig from '../../eslint.config.mjs';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(...baseConfig, {
