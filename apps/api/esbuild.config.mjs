361fb07dad675ba686cf3abf771682dc546d3a83
Patrick Rodrigues (rodapdev) <47316946+RodPaDev@users.noreply.github.com>
2025-05-27 10:25:35 +0100

    fix: update import assertion syntax and set package type to module in package.json

--- apps/api/esbuild.config.mjs
+++ apps/api/esbuild.config.mjs
@@ -1,5 +1,5 @@
 import { build } from "esbuild";
-import pkg from "./package.json" assert { type: "json" };
+import pkg from "./package.json" with { type: "json" };
 
 build({
   entryPoints: ["./src/**/*.ts"],
