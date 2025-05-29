4897b9911a93b6d5b8610ed2235077f07b07b656
Patrick Rodrigues (rodapdev) <47316946+RodPaDev@users.noreply.github.com>
2025-05-29 15:54:05 +0100

    add tanstack router + example routes

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,11 +1,27 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
+
+import { routeTree } from "@/routeTree.gen";
+
+import "@/index.css";
 import { createRoot } from "react-dom/client";
 
-import App from "@/App.tsx";
-import "@/index.css";
+const router = createRouter({
+  routeTree,
+});
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+}
---
+++ apps/client/src/routes/organizations/$organizationId/actions/new.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute(
+  '/organizations/$organizationId/actions/new',
+)({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <div>Hello "/organizations/$organizationId/actions/new"!</div>
+}
