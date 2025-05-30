6019c06b8f0d38c17515d041839df527e856d560
Patrick Rodrigues (rodapdev) <47316946+RodPaDev@users.noreply.github.com>
2025-05-30 15:37:47 +0100

    feat: enhance HTML structure and integrate dynamic head content management

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -1,12 +1,16 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, HeadContent, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 import { Fragment } from "react/jsx-runtime";
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [{ title: "fsm" }],
+  }),
   component: () => {
     return (
       <Fragment>
+        <HeadContent />
         <Outlet />
         <TanStackRouterDevtools position="bottom-right" />
       </Fragment>
