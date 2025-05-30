6bf804592c6cdbc10ea3816209d679f53b2e54fc
João Cosme <mrtuga49@gmail.com>
2025-05-30 16:38:56 +0100

    chore: add traanslations json

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,3 +1,6 @@
+import "@/index.css";
+import { routeTree } from "@/routeTree.gen";
+import enTranslations from "@/translations/en.json";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import i18n from "i18next";
@@ -5,40 +8,26 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { initReactI18next } from "react-i18next";
 
-import { routeTree } from "@/routeTree.gen";
-
-import "@/index.css";
-
 const queryClient = new QueryClient();
 
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
-    // the translations
-    // (tip move them in a JSON file and import them,
-    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-    resources: {
-      en: {
-        translation: {
-          allInbox: "Welcome to React and react-i18next",
-        },
-      },
-    },
-    lng: "en", // if you're using a language detector, do not define the lng option
-    fallbackLng: "en",
-
-    interpolation: {
-      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
-    },
-    saveMissing: true,
-    saveMissingTo: "all", // send to all languages, not just current
-    missingKeyHandler: (lng, ns, key, fallbackValue) => {
-      // UNCOMMENT THIS TO LOG MISSING TRANSLATIONS IN BROWSER CONSOLE
-      // console.log(
-      //   `Missing translation - Language: ${lng}, Namespace: ${ns}, Key: ${key}`,
-      // );
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources: {
+    en: enTranslations,
+  },
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+  saveMissing: true,
+  saveMissingTo: "all",
+  missingKeyHandler: (_lng, _ns, _key, _fallbackValue) => {
+    // UNCOMMENT THIS TO LOG MISSING TRANSLATIONS IN BROWSER CONSOLE
+    // console.log(
+    //   `Missing translation - Language: ${lng}, Namespace: ${ns}, Key: ${key}`,
+    // );
+  },
+});
 
 const router = createRouter({
   routeTree,
