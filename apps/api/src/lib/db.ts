eb6b5082cc50c1f392fec6694be102b4d62cb39b
Patrick Rodrigues (rodapdev) <47316946+RodPaDev@users.noreply.github.com>
2025-05-30 15:14:06 +0100

    feat: establish database connection using Pool and enhance error handling

--- apps/api/src/lib/db.ts
+++ apps/api/src/lib/db.ts
@@ -2,8 +2,21 @@ import { env } from "@/env";
 import * as schema from "@/models";
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-export const db = drizzle({
-  schema,
-  connection: env.DATABASE_URL,
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+  connectionTimeoutMillis: 2000,
 });
+
+try {
+  // Attempt to establish a database connection before proceeding.
+  // This ensures the API only starts if the database is reachable.
+  await pool.connect();
+} catch (error) {
+  console.error("Failed to connect to the database");
+  console.error(error);
+  process.exit(1);
+}
+
+export const db = drizzle(pool, { schema });
